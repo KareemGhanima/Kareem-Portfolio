cfb0ade8d389389bbf18702f8fa5a115e2e605af
Kareem Ghanima <kareemwalidghanima@gmail.com>
2026-08-20 19:27:04 +0300

    Update Kareem portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -27,7 +27,7 @@ const greeting = {
   subTitle: emoji(
     "A Mechatronics Engineering student focused on Industrial Automation, Artificial Intelligence, Robotics, Control Systems, and Computer Vision. ⚙️🤖"
   ),
-  resume_url: "https://drive.google.com/file/d/1DClcNFFraw5QVllZt_D9_FlGRO8q2Who/view?usp=drive_link",
+  DOWNLOAD_MY_RESUME_url: "https://drive.google.com/file/d/1DClcNFFraw5QVllZt_D9_FlGRO8q2Who/view?usp=drive_link",
   displayGreeting: true
 };
 
